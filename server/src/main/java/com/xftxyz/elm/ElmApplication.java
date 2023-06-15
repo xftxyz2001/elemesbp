33de6c10bcf2d69adec6b009bb3a9b38dbc7bbff
通晓宇宙 <2581011320@qq.com>
2023-06-15 16:12:36 +0800

    springboot init

---
+++ server/src/main/java/com/xftxyz/elm/ElmApplication.java
@@ -0,0 +1,13 @@
+package com.xftxyz.elm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElmApplication.class, args);
+    }
+
+}
