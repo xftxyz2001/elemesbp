b1828f90beabbbf3520fb8b8cf18f2e4f0bdf7b6
通晓宇宙 <2581011320@qq.com>
2023-06-16 20:50:00 +0800

    font-awesome

---
+++ frontend/src/components/FooterSection.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <ul class="footer">
+    <li>
+      <i class="fa fa-home"></i>
+      <p>首页</p>
+    </li>
+    <li>
+      <i class="fa fa-compass"></i>
+      <p>发现</p>
+    </li>
+    <li>
+      <i class="fa fa-file-text-o"></i>
+      <p>订单</p>
+    </li>
+    <li>
+      <i class="fa fa-user-o"></i>
+      <p>我的</p>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.wrapper .footer {
+  width: 100%;
+  height: 14vw;
+  border-top: solid 1px #DDD;
+  background-color: #fff;
+
+  position: fixed;
+  left: 0;
+  bottom: 0;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.wrapper .footer li {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  color: #999;
+  user-select: none;
+  cursor: pointer;
+}
+
+.wrapper .footer li p {
+  font-size: 2.8vw;
+}
+
+.wrapper .footer li i {
+  font-size: 5vw;
+}
+</style>
