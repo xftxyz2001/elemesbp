22e54be7629db245e977d68964fc3bb811fabd62
通晓宇宙 <2581011320@qq.com>
2023-07-07 21:49:42 +0800

    新的日志拦截器

--- server/src/main/java/com/xftxyz/elm/interceptor/LoggingInterceptor.java
+++ server/src/main/java/com/xftxyz/elm/interceptor/LoggingInterceptor.java
@@ -1,36 +1,34 @@
 package com.xftxyz.elm.interceptor;
 
-import java.util.Arrays;
 import java.util.Map;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 日志：打印请求地址和参数
+ */
 @Slf4j
 public class LoggingInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        log.info("Request URL: {}", request.getRequestURL());
-        log.info("Request Method: {}", request.getMethod());
-        log.info("Request Parameters: {}", getParameters(request));
-        return true;
-    }
-
-    private String getParameters(HttpServletRequest request) {
         Map<String, String[]> parameterMap = request.getParameterMap();
-        if (parameterMap.isEmpty()) {
-            return "";
-        }
-        StringBuilder sb = new StringBuilder();
+        StringBuffer parms = new StringBuffer();
         for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-            String key = entry.getKey();
-            String[] values = entry.getValue();
-            sb.append(key).append("=").append(Arrays.toString(values)).append("&");
+            for (String value : entry.getValue()) {
+                parms.append(entry.getKey()).append("=").append(value).append("&");
+            }
         }
-        return sb.deleteCharAt(sb.length() - 1).toString();
+
+        log.info("{}:{}", request.getMethod(), request.getRequestURL());
+        if (StringUtils.hasLength(parms)) {
+            log.info("{}", parms.substring(0, parms.length() - 1));
+        }
+        return true;
     }
 }
