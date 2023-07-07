600b4f8a85b1dd69aa9226e390ad22d543119f29
通晓宇宙 <2581011320@qq.com>
2023-07-07 20:10:10 +0800

    路由

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,54 +8,70 @@ import RegisterView from '@/views/RegisterView.vue' // 注册页
 import BusinessListView from '@/views/BusinessListView.vue'; // 商家列表页
 import BusinessInfoView from '@/views/BusinessInfoView.vue'; // 商家详情页
 import OrderView from '@/views/OrderView.vue'; // 订单页
+import UserAddressView from '@/views/UserAddressView.vue'; // 用户地址页
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      // 首页
       path: '/',
       name: 'home',
       component: HomeView
     },
     {
+      // 发现页
       path: '/discover',
       name: 'discover',
       component: DiscoverView
     },
     {
+      // 订单列表页
       path: '/myorder',
       name: 'myorder',
       component: MyOrdersView
     },
     {
+      // 我的页
       path: '/profile',
       name: 'profile',
       component: ProfileView
     },
     {
+      // 登录页
       path: '/login',
       name: 'login',
       component: LoginView
     },
     {
+      // 注册页
       path: '/register',
       name: 'register',
       component: RegisterView
     },
     {
+      // 商家列表页
       path: '/businessList/:type',
       name: 'businessList',
       component: BusinessListView
     },
     {
-      path: '/businessInfo/:id',
+      // 商家详情页
+      path: '/businessInfo',
       name: 'businessInfo',
       component: BusinessInfoView
     },
     {
+      // 确认订单页
       path: '/order/:id',
       name: 'order',
       component: OrderView
+    },
+    {
+      // 用户地址页
+      path: '/useraddress',
+      name: 'userAddress',
+      component: UserAddressView
     }
     // {
     //   path: '/about',
