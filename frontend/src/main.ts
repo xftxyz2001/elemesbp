035044f2ff46377b40ff130c9e6513ae636d9fb9
通晓宇宙 <2581011320@qq.com>
2023-07-09 11:50:19 +0800

    format

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,32 +1,31 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import axios from 'axios'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import axios from 'axios';
 
-import App from './App.vue'
-import router from './router'
-import 'font-awesome/css/font-awesome.min.css'
+import App from './App.vue';
+import router from './router';
+import 'font-awesome/css/font-awesome.min.css';
 
-axios.defaults.baseURL = 'http://localhost:8080'
-axios.defaults.withCredentials = true
-// axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
-const app = createApp(App)
-router.beforeEach(function(to,from,next){
+axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.withCredentials = true;
+
+const app = createApp(App);
+router.beforeEach(function (to, from, next) {
     console.log(to.path);
     //除了首页，商家列表，商家信息，注册，登录之外都得判断是否登录
-    if(!(to.path=='/' || to.path.startsWith('/businessList') || to.path=='/businessInfo' || to.path=='/login' || to.path=='/register')){
-        axios.get('/user/info').then((res)=>{
+    if (!(to.path == '/' || to.path.startsWith('/businessList') || to.path == '/businessInfo' || to.path == '/login' || to.path == '/register')) {
+        axios.get('/user/info').then((res) => {
             const r = res.data;
-            if(r.code == 100){
-                router.push({name:'login'});
+            if (r.code == 100) {
+                router.push({ name: 'login' });
             }
-        })
+        });
     }
     next();
 });
 
-
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
