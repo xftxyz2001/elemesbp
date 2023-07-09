a2d2ca48415690eff5a657d4522629de2e772678
通晓宇宙 <2581011320@qq.com>
2023-07-09 13:55:19 +0800

    参数校验

--- server/src/main/java/com/xftxyz/elm/vo/req/CartUpdateVO.java
+++ server/src/main/java/com/xftxyz/elm/vo/req/CartUpdateVO.java
@@ -1,5 +1,8 @@
 package com.xftxyz.elm.vo.req;
 
+import com.xftxyz.elm.validation.ValidInfo;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -7,15 +10,18 @@ public class CartUpdateVO {
     /*
      * 商家id
      */
+    @NotBlank(message = ValidInfo.BUSINESS_ID_NOT_NULL)
     private Integer businessid;
 
     /*
      * 食品id
      */
+    @NotBlank(message = ValidInfo.FOOD_ID_NOT_NULL)
     private Integer foodid;
 
     /*
      * 数量
      */
+    @NotBlank(message = ValidInfo.QUANTITY_NOT_NULL)
     private Integer quantity;
 }
